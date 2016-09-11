3f8dc2023f9422b8633302cdf6ecfeee815e3eb6
Stephen Simmons <mail@stevesimmons.com>
2016-09-11 23:07:57 +0100

    Adding Jupyter notebooks for examples 1 and 2, and data files

--- usb-receiver.py
+++ usb-receiver.py
@@ -23,6 +23,7 @@ Stephen Simmons - 5 Sep 2016
 https://github.com/stevesimmons/pyconuk-motion-sensor-data-fusion
 """
 
+import logging
 import serial
 import serial.tools.list_ports
 import sys
@@ -30,29 +31,37 @@ import sys
 
 def main():
     port = find_microbit()
-    print_from_usb(port)
+    if port:
+        print_from_usb(port)
+    else:
+        sys.stderr.write("Unable to find a microbit on any USB port\n")
+    
 
 
 def find_microbit(serial_number=None):
     "Name of the first matching micro:bit's serial port, otherwise None."
     for port in serial.tools.list_ports.comports():
-        if port.product == 'MBED CMSIS-DAP':
+        sys.stderr.write("Checking %s" % port.description)
+        if "CMSIS-DAP" in port.product:
             if serial_number is None or serial_number == port.serial_number:
+                sys.stderr.write("Found microbit %s on %s\n" % (port.serial_number, port.device))
                 return port.device
     return None
 
 
 def print_from_usb(port, out=sys.stdout):
     "Write serial data from USB port to a file, by default sys.stdout."
-    ser = serial.Serial(port, baudrate=115200)
-    try:
-        while True:
-            line = ser.readline().decode()
-            out.write(line)
-    except:
-        pass
-    finally:
-        ser.close()
+    with serial.Serial(port, baudrate=115200, timeout=20) as ser:
+        try:
+            while True:
+                line = ser.readline().decode()
+                out.write(line)
+        except KeyboardInterrupt:
+            pass
+        except:
+            logging.exception("Error")
+        finally:
+            ser.close()
 
 
 if __name__ == '__main__':
